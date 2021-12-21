9770008f8457a8b87c8c57b04a114aae2319ddc5
shivesh-ranjan <ranjanshivesh71@gmail.com>
2021-12-21 15:49:08 +0000

    fibonacci

---
+++ College Homework/Fibonacci/fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+
+    int i, n;
+
+    int t1 = 0, t2 = 1;
+
+    int nextterm = t1 + t2;
+    printf("Enter the number of terms: \n");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+    for (i = 3; i <= n; ++i) {
+        printf("%d, ", nextterm);
+        t1 = t2;
+        t2 = nextterm;
+        nextterm = t1 + t2;
+    }
+
+    return 0;
+}
