1eabdc197b54e4bbbd7545edbf938caefc0ff67c
shivesh-ranjan <ranjanshivesh71@gmail.com>
2022-01-21 06:06:40 +0000

    cb vow conso

---
+++ LABS/Beyond Syllabus/CB vow Conso.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char c;
+    int vowel;
+    printf("Enter an alphabet: ");
+    scanf("%c", &c);
+    vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // evaluates to 1 (true) if c is a vowel
+    if (vowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
+}
