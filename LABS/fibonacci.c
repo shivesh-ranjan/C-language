38cc15c4613bad49cf06af367ace146423f59c55
Shivesh Ranjan <86239697+shivesh-ranjan@users.noreply.github.com>
2022-03-21 21:40:05 +0530

    Update fibonacci.c

--- LABS/fibonacci.c
+++ LABS/fibonacci.c
@@ -1,26 +1,16 @@
-#include <stdio.h>
-int main() {
+//Fibonacci Series using Recursion
+#include<stdio.h>
+int fib(int n)
+{
+if (n <= 1)
+	return n;
+return fib(n-1) + fib(n-2);
+}
 
-    int i, n;
-
-  // Initial first and second terms
-    int t1 = 0, t2 = 1;
-
-  // Third Term
-    int nextterm = t1 + t2;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-  // print the first two terms t1 and t2
-    printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-  // print 3rd to nth terms
-    for (i = 3; i <= n; ++i) {
-        printf("%d, ", nextterm);
-        t1 = t2;
-        t2 = nextterm;
-        nextterm = t1 + t2;
-    }
-
-    return 0;
+int main ()
+{
+int n = 9;
+printf("%d", fib(n));
+getchar();
+return 0;
 }
